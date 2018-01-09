38c4808cae0c7c77a469c0d3be498d2aa38dfaab
boonprakit <boonprakit@proteus-tech.com>
2018-01-10 01:06:16 +0700

    show modal

--- App.js
+++ App.js
@@ -27,15 +27,21 @@ import {
   Platform,
   StyleSheet,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  Dimensions,
+  Image
 } from 'react-native';
 import {signIn} from './src/redux/actions.js'
 import {store} from './src/redux'
+import Drawer from 'react-native-drawer'
 
 import ProfileSettings from './src/screens/profile'
 import ChatList from './src/screens/chatlist'
 import Contacts from './src/screens/contact'
 import Chat from './src/screens/chat'
+
+const deviceHeight = Dimensions.get("window").height
+const deviceWidth = Dimensions.get("window").width
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -59,6 +65,10 @@ export default class App extends Component<{}> {
 
 
     }
+
+    openControlPanel = () => {
+		this._drawer.open()
+	}
 
     onTabClick(selectedTab) {
         this.setState({
@@ -84,6 +94,41 @@ export default class App extends Component<{}> {
 
     render() {
         return (
+            <Drawer
+    	        ref={(ref) => this._drawer = ref}
+    	        type="overlay"
+    	        tapToClose={true}
+    	        openDrawerOffset={0.2} // 20% gap on the right side of drawer
+    	        panCloseMask={0.2}
+    	        closedDrawerOffset={-3}
+    	        styles={drawerStyles}
+    	        tweenHandler={(ratio) => ({
+    	            main: { opacity:(2-ratio)/2}
+    	        })}
+    	        content=
+    	        {
+    	            <Container>
+    					<Content bounces={false} style={{ flex: 1, backgroundColor: "#fff", top: -1 }}>
+    						<View  style={styles.drawerCover}>
+    							<Image square style={styles.drawerImage} source={  require('./src/assets/logo.png') } />
+    						</View>
+    						<List>
+    							<ListItem itemHeader first style={{ paddingBottom: 3 }}>
+    								<Text>ACCOUNT</Text>
+    							</ListItem>
+    							<ListItem button noBorder onPress={() => this._drawer.close()}>
+    								<Left>
+    									<Icon active name='log-out' style={{ color: "#777", fontSize: 26, width: 30 }} />
+    									<Text style={styles.text}>
+    										Log Out
+    									</Text>
+    								</Left>
+    							</ListItem>
+    						</ List>
+    					</Content>
+    				</Container>
+    	        }
+	    >
             <Container>
               <Header style={{ backgroundColor: '#3b5998' }}>
                   <Left>
@@ -95,9 +140,6 @@ export default class App extends Component<{}> {
                       <Title>{ this.state.selectedTab }</Title>
                   </Body>
                   <Right>
-                      <Button transparent onPress={() => this.props.navigation.navigate('AddFriend')}>
-                          <Icon name="md-person-add" />
-                      </Button>
                   </Right>
               </Header>
               <Content>
@@ -123,8 +165,14 @@ export default class App extends Component<{}> {
                 </FooterTab>
               </Footer>
             </Container>
+        </Drawer>
         );
     }
+}
+
+const drawerStyles = {
+     drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3},
+     main: {paddingLeft: 3},
 }
 
 const styles = StyleSheet.create({
@@ -144,4 +192,23 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  drawerCover: {
+		alignSelf: "stretch",
+		// resizeMode: 'cover',
+		height: deviceHeight / 3.5,
+		width: null,
+		position: "relative",
+		marginBottom: 10,
+		backgroundColor: '#004B85'
+	},
+	drawerImage: {
+		position: "absolute",
+		// left: (Platform.OS === 'android') ? 30 : 40,
+		left: Platform.OS === "android" ? deviceWidth / 15 : deviceWidth / 12,
+		// top: (Platform.OS === 'android') ? 45 : 55,
+		top: Platform.OS === "android" ? deviceHeight / 13 : deviceHeight / 12,
+		width: 250,
+		height: 75,
+		resizeMode: "cover"
+	}
 });
