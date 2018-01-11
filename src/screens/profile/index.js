61850312f98a3f0a77fa75710869de36d670ee60
boonprakit <boonprakit@proteus-tech.com>
2018-01-12 01:41:48 +0700

    can pick profile image

--- src/screens/profile/index.js
+++ src/screens/profile/index.js
@@ -17,8 +17,19 @@ import {
 import {data} from '../../data';
 import {FontAwesome} from '../../assets/icons';
 import { Thumbnail, Button, Text, Icon } from 'native-base';
-
+import ImagePicker from 'react-native-image-picker'
 import {store} from '../../redux'
+
+var options = {
+  title: 'Select Avatar',
+  customButtons: [
+    {name: 'fb', title: 'Choose Photo from Facebook'},
+  ],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images'
+  }
+};
 
 
 export default class ProfileSettings extends React.Component {
@@ -62,34 +73,84 @@ export default class ProfileSettings extends React.Component {
                 style={{width: '100%', height: 150, borderTopLeftRadius: 4, borderTopRightRadius: 4 }}
                 source={{uri: this.state.wall_pic_url}}
             />
+            <Button
+                transparent
+                dark
+                style={{
+                    position: 'absolute',
+                    top: 5,
+                    right: 5,
+                    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+                }}
+            >
+                <Text>UPDATE WALL PHOTO</Text>
+            </Button>
             <TouchableHighlight
-            style={{
-                width: 110,
-                height: 110,
-                borderRadius: 55,
-                borderColor: 'white',
-                borderWidth: 1,
-                position: 'absolute',
-                top: 95,
-                left: '50%',
-                marginLeft: -55
-            }}
-             onPress={() => alert('hello')}>
-                <Image
-                    style={{
+                style={{
                         width: 110,
                         height: 110,
-                        borderRadius: 55
+                        borderRadius: 55,
+                        borderColor: 'white',
+                        borderWidth: 1,
+                        position: 'absolute',
+                        top: 95,
+                        left: '50%',
+                        marginLeft: -55
                     }}
-                    source={{uri: this.state.profile_pic_url}}
+                 onPress={() => {
+                    ImagePicker.showImagePicker(options, (response) => {
+                        console.log('Response = ', response);
 
-                />
+                        if (response.didCancel) {
+                            console.log('User cancelled image picker')
+                        }
+                        else if (response.error) {
+                            console.log('ImagePicker Error: ', response.error)
+                        }
+                        else if (response.customButton) {
+                            console.log('User tapped custom button: ', response.customButton)
+                        }
+                        else {
+                            // let source = { uri: response.uri }
+
+                            // You can also display the image using data:
+                            // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+                            this.setState({
+                                profile_pic_url: response.uri
+                            })
+                        }
+                    })
+             }}>
+                <View>
+                    <Image
+                        style={{
+                            width: 110,
+                            height: 110,
+                            borderRadius: 55
+                        }}
+                        source={{uri: this.state.profile_pic_url}}
+                    />
+                    <Button
+                        block
+                        transparent
+                        dark
+                        style={{
+                            position: 'absolute',
+                            width: 100,
+                            height: 30,
+                            marginLeft: -50,
+                            bottom: 0,
+                            left: '50%',
+                            zIndex: 999,
+                            backgroundColor: 'rgba(255, 255, 255, 0.4)',
+                        }}>
+                        <Text>EDIT</Text>
+                    </Button>
+                </View>
             </TouchableHighlight>
         </View>
         <View>
-            <Button block bordered>
-                <Text>Info</Text>
-            </Button>
         </View>
           <View style={styles.section}>
             <View style={[styles.row, styles.heading]}>
