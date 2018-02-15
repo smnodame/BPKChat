3b158f589e4c7b056aaa0fec6cf628e4e1e2084b
boonprakit <boonprakit@proteus-tech.com>
2018-02-16 03:24:41 +0700

    can get shareText from other app

--- android/app/src/main/java/com/bpkchat/MainApplication.java
+++ android/app/src/main/java/com/bpkchat/MainApplication.java
@@ -3,6 +3,7 @@ package com.bpkchat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.meedan.ShareMenuPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.imagepicker.ImagePickerPackage;
 import io.realm.react.RealmReactPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ShareMenuPackage(),
             new RNFetchBlobPackage(),
             new ImagePickerPackage(),
             new RealmReactPackage(),
--- src/screens/splash/index.js
+++ src/screens/splash/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StyleSheet, Image, View, AsyncStorage } from 'react-native';
+import { StyleSheet, Image, View, AsyncStorage } from 'react-native'
 import { NavigationActions } from 'react-navigation'
+import ShareMenu from 'react-native-share-menu'
 
 import { signin, start_app, navigate, enterSplash  } from '../../redux/actions.js'
 import {store} from '../../redux'
+
 
 export default class Splash extends React.Component {
     constructor(props) {
@@ -12,40 +14,14 @@ export default class Splash extends React.Component {
     }
 
     async componentDidMount() {
-        // const host = await AsyncStorage.getItem('default_host')
-		// if(host) {
-        //
-		// } else {
-		// 	await AsyncStorage.setItem('default_host', 'http://192.168.13.31/bpkservice')
-		// }
-        //
-        // const token = await AsyncStorage.getItem('token')
-		// if(token) {
-		// 	const current_state = await AsyncStorage.getItem('current_state')
-        //     setTimeout(() => {
-        //         const resetAction = NavigationActions.reset({
-        //                 index: 0,
-        //                 actions: [
-        //                     NavigationActions.navigate({ routeName: current_state})
-        //                 ]
-        //             })
-        //         this.props.navigation.dispatch(resetAction)
-        //     }, 1500)
-		// } else {
-			// await AsyncStorage.setItem('current_state', 'Login')
-			// await AsyncStorage.removeItem('patient_host')
-            // await AsyncStorage.removeItem('pos_host')
-            // setTimeout(() => {
-            //     const resetAction = NavigationActions.reset({
-            //             index: 0,
-            //             actions: [
-            //                 NavigationActions.navigate({ routeName: 'Login'})
-            //             ]
-            //         })
-            //     this.props.navigation.dispatch(resetAction)
-            // }, 1500)
-		// }
-		store.dispatch(enterSplash())
+        ShareMenu.getSharedText((text :string) => {
+            if (text && text.length) {
+                console.log('text')
+                console.log(text)
+            } else {
+                store.dispatch(enterSplash())
+            }
+        })
     }
 
     render() {
