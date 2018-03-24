07dea962ca963294906c98e499ec58fff2ab569b
boonprakit <boonprakit@proteus-tech.com>
2018-03-25 03:48:30 +0700

    create calling page and layout

--- android/app/src/main/java/com/bpkchat/MainApplication.java
+++ android/app/src/main/java/com/bpkchat/MainApplication.java
@@ -3,6 +3,7 @@ package com.bpkchat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oney.WebRTCModule.WebRTCModulePackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.filepicker.FilePickerPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new WebRTCModulePackage(),
             new RNSoundPackage(),
             new ReactNativeAudioPackage(),
             new FilePickerPackage(),
